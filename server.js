7f674ea26d8e73645e4e0d52c789c04f8c2f0f87
akimender <akimender@gmail.com>
2024-08-21 22:08:00 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+const PORT = process.env.PORT | 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
 });
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(PORT, () => console.log('Example app is listening on port 3000.'));
