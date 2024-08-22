1ee73281a5d14dba59a09a11ed9744d9e5a4e8de
akimender <akimender@gmail.com>
2024-08-21 22:02:57 -0400

    done

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
